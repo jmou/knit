35e1a5a6a86c61c8abf803379e1c051cfbb5e129
Joe Mou <dev@mou.fo>
2020-12-28 19:25:31 -0800

    make: pattern rules

--- make/run.sh
+++ make/run.sh
@@ -2,5 +2,9 @@ mkdir -p in/inputs
 cd in/inputs
 bash -ex ../recipe
 while read -r target; do
-    mv $target ../../out/
+    if [ -f $target ]; then
+        mv $target ../../out/
+    else
+        echo "warn: target '$target' not written" >&2
+    fi
 done < ../targets
