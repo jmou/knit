357b822b1810bdd25fbb681aed3804fc2fe6cf63
Joe Mou <dev@mou.fo>
2024-10-24 23:36:50 -0400

    Mitigate session lock contention

--- knit-resume-session.sh
+++ knit-resume-session.sh
@@ -28,13 +28,24 @@ while [[ $(knit-list-steps --available --blocked "$session" | wc -l) -gt 0 ]]; d
         echo "Dispatch $job step $name" >&3
         {
             prd=$(knit-dispatch-job $verbose "$job")
-            knit-complete-job "$session" "$job" "$prd"
+            echo "$prd" > "$KNIT_DIR/scratch/tmp-$session.$job"
+            mv "$KNIT_DIR/scratch/tmp-$session.$job" "$KNIT_DIR/scratch/$session.$job"
         } &
     done < <(knit-list-steps --available --porcelain "$session")
 
     # When we have scheduled everything we can, wait for knit-dispatch-job.
     if [[ -z $has_scheduled ]]; then
         wait -n
+        # While it is possible to run knit-complete-job in the child process
+        # with knit-dispatch-job, this results in session lock contention.
+        # Instead we collect the results and run in serial.
+        for i in "$KNIT_DIR/scratch/$session."*; do
+            [[ -e $i ]] || continue
+            job="${i#$KNIT_DIR/scratch/$session.}"
+            prd="$(< "$i")"
+            knit-complete-job "$session" "$job" "$prd"
+            rm "$i"
+        done
     fi
 done
 
