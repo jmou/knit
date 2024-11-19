f68451f8d837802eebef78873788627ffecacb17
Joe Mou <dev@mou.fo>
2024-11-18 21:36:29 -0500

    Fix PATH modification
    We should only modify PATH if the knit command is called with an
    absolute or relative path. Otherwise we would incorrectly add the
    current working directory to PATH.
    
    In the future we may replace this all with a compile-time KNIT_EXEC_PATH
    similar to git. This could especially be the case if we install
    subcommands to /usr/libexec.

--- knit.c
+++ knit.c
@@ -1,6 +1,21 @@
 #include "util.h"
 
-void die_usage(char* arg0) {
+static void prepend_path(char* dir) {
+    char* env_path = getenv("PATH");
+    if (env_path) {
+        char* newpath = xmalloc(strlen(dir) + strlen(env_path) + 2);
+        sprintf(newpath, "%s:%s", dir, env_path);
+        env_path = newpath;
+    } else {
+        env_path = dir;
+    }
+    if (setenv("PATH", env_path, 1) < 0) {
+        perror("setenv");
+        exit(1);
+    }
+}
+
+static void die_usage(char* arg0) {
     fprintf(stderr, "usage: %s [-C <dir>] <command> [<args>]\n", arg0);
     exit(1);
 }
@@ -8,11 +23,12 @@ void die_usage(char* arg0) {
 int main(int argc, char** argv) {
     // We are about to exec() so it's fine (even preferable) to leak memory.
 
-    char* bin_dir = realpath(dirname(argv[0]), NULL);
-    if (!bin_dir) {
-        perror("realpath");
-        exit(1);
+    if (strchr(argv[0], '/')) {
+        char* bin_dir = realpath(dirname(strdup(argv[0])), NULL);
+        if (bin_dir)
+            prepend_path(bin_dir);
     }
+    // If argv[0] is bare, $PATH does not need modification.
 
     int argi = 1;
     while (argv[argi] && *argv[argi] == '-') {
@@ -29,19 +45,6 @@ int main(int argc, char** argv) {
     }
     if (argc < argi + 1)
         die_usage(argv[0]);
-
-    char* env_path = getenv("PATH");
-    if (env_path) {
-        char* newpath = xmalloc(strlen(bin_dir) + strlen(env_path) + 2);
-        sprintf(newpath, "%s:%s", bin_dir, env_path);
-        env_path = newpath;
-    } else {
-        env_path = bin_dir;
-    }
-    if (setenv("PATH", env_path, 1) < 0) {
-        perror("setenv");
-        exit(1);
-    }
 
     char* subcmd = xmalloc(strlen(argv[argi]) + 6);
     sprintf(subcmd, "knit-%s", argv[argi]);
