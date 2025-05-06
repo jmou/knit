jmou/knit

commit def451566cbba67527b181c296b08441f564cb52

Rework job scheduling
We previously had a decentralized approach to job scheduling relying on
fine-grained locking, but this had concurrency races. It was also
implemented synchronously, which would not work well with manual job
processes.

We introduce knit-schedule-jobs, which absorbs the job dispatch
functionality (from knit-resume-session) and some flow job session
management (from knit-dispatch-job). Additionally it routes job
dispatches and production results across sessions.

Communication with knit-resume-session uses a simple line-based
protocol (hence buffered IO must be line buffered). knit-resume-session
requests jobs to be scheduled by writing them to stdout; it receives
corresponding productions on stdin. In turn, knit-schedule-jobs
dispatches requested jobs, keeps track of the requesting sessions, and
notifies them of completed productions.

To annotate jobs with additional data, we add a word-sized extra field
to struct object. knit-schedule-jobs stores job dispatch state, and
knit-resume-session stores associated steps.

Remove the settling loop in knit-resume-session, which appears to be
vestigial from when the session could be modified during scheduling.

This regresses job caching, because flow jobs no longer are saved to the
cache. We probably should move cache handling into knit-schedule-jobs.

knit-dispatch-job.sh
@@ -109,9 +109,8 @@ while read -r input; do
         else
             knit-parse-plan --build-instructions "$job" | knit-build-session "$session"
         fi
-        inv=$(knit-resume-session "$session")
-        prd=$(knit-remix-production --set-job "$job" --wrap-invocation "$inv")
-        finish
+        echo session
+        exit
     elif [[ $input == .knit/identity ]]; then
         prd=$(knit-remix-production --set-job "$job" --copy-job-inputs "$job" \
             --remove-prefix .knit/ --set-output ".knit/ok=$(empty_res)")

knit-schedule-jobs.c
@@ -1,0 +1,400 @@
+// Coordinate and dispatch jobs across sessions.
+//
+// All jobs are initially dispatched through knit-dispatch-job, which handles
+// them in one of two ways:
+// - Simple jobs are executed and return their productions.
+// - Flow jobs set up sessions. For each session, we establish a two-way pipe
+//   with knit-resume-session to receive job scheduling requests and respond
+//   with their productions. When the session is complete we wrap its invocation
+//   in a production.
+//
+// Each outstanding job, or dispatch, maintains which sessions are waiting upon
+// it. Upon completion, we notify these sessions of the resulting production.
+
+#include "hash.h"
+#include "job.h"
+#include "spec.h"
+#include "util.h"
+
+#include <poll.h>
+#include <signal.h>
+
+#define MAX_DISPATCHES 1024
+
+// Atomically open a lock file and return its file descriptor, or -1 on error.
+// TODO refactor session.c
+static int acquire_lockfile(const char* lockfile) {
+    static int is_rand_seeded = 0;
+    if (!is_rand_seeded) {
+        srand(getpid());
+        is_rand_seeded = 1;
+    }
+
+    int fd;
+    long backoff_ms = 1;
+    while ((fd = open(lockfile,
+                      O_WRONLY | O_CREAT | O_EXCL | O_CLOEXEC, 0666)) < 0) {
+        if (errno != EEXIST)
+            return error_errno("open error %s", lockfile);
+
+        backoff_ms *= 2;
+        if (backoff_ms > 5000)
+            return error("lockfile timed out: %s", lockfile);
+        // Randomly perturb backoff by +/-25% and scale to microseconds.
+        long sleep_us = backoff_ms * (750 + rand() % 500);
+        usleep(sleep_us);
+    }
+
+    return fd;
+}
+
+static char sched_lockfile[PATH_MAX];
+
+static void unlock_sched() { unlink(sched_lockfile); }
+
+// TODO drain dispatches instead of abandoning them
+static void sighandler(int signo) {
+    exit(128 + signo);  // will run atexit handlers
+}
+
+enum dispatch_state {
+    DS_INITIAL = 0,
+    DS_RUNNING,
+    DS_PENDING_SESSION,
+    DS_SESSION,
+    DS_LAMEDUCK,
+    DS_DONE,
+};
+
+struct notify_list {
+    // Each session is identified by its flow job.
+    struct job* flow_job;
+    struct notify_list* next;
+};
+
+// Parallels a pollfd that listens for process output. Stored in the job object
+// extra field.
+struct dispatch {
+    enum dispatch_state state;
+    pid_t pid;
+    int writefd;  // for DS_SESSION state
+    struct notify_list* notify;
+    char buf[KNIT_HASH_HEXSZ + 20];
+};
+
+static void free_dispatch(struct dispatch* d) {
+    if (d->notify) {
+        error("abandoning dispatch with notifications");
+        struct notify_list* s = d->notify;
+        while (s) {
+            struct notify_list* tmp = s->next;
+            free(s);
+            s = tmp;
+        }
+    }
+    free(d);
+}
+
+static void dispatch_add_session(struct dispatch* dispatch,
+                                 struct job* flow_job) {
+    struct notify_list* list = xmalloc(sizeof(*list));
+    list->flow_job = flow_job;
+    list->next = dispatch->notify;
+    dispatch->notify = list;
+}
+
+static int dispatch_is_dead(struct dispatch* dispatch) {
+    return dispatch->state == DS_DONE;
+}
+
+static void pipe_cloexec(int fd[2]) {
+    if (pipe(fd) < 0)
+        die_errno("pipe failed");
+    fcntl(fd[0], F_SETFD, FD_CLOEXEC);
+    fcntl(fd[1], F_SETFD, FD_CLOEXEC);
+}
+
+static int spawn(struct dispatch* dispatch, char** argv, int pipe_stdin) {
+    assert(dispatch->pid == 0);
+    assert(dispatch->writefd == -1);
+
+    int infd[2];
+    int outfd[2];
+    pipe_cloexec(outfd);
+    if (pipe_stdin)
+        pipe_cloexec(infd);
+
+    pid_t pid = fork();
+    if (pid < 0)
+        die_errno("fork failed");
+    if (!pid) {
+        dup2(outfd[1], STDOUT_FILENO);
+        if (pipe_stdin)
+            dup2(infd[0], STDIN_FILENO);
+        else
+            close(STDIN_FILENO);
+        execvp(argv[0], argv);
+        die_errno("execvp failed");
+    }
+    close(outfd[1]);
+    if (pipe_stdin) {
+        close(infd[0]);
+        dispatch->writefd = infd[1];
+    }
+    dispatch->pid = pid;
+    return outfd[0];
+}
+
+static void dispatch_job(struct job* job, int* fd) {
+    struct dispatch* dispatch = job->object.extra;
+    assert(dispatch);
+    dispatch->state = DS_RUNNING;
+    char* argv[] = { "knit-dispatch-job", oid_to_hex(&job->object.oid), NULL };
+    *fd = spawn(dispatch, argv, 0);
+}
+
+static int handle_eof(struct job* job, int* fd) {
+    struct dispatch* dispatch = job->object.extra;
+    assert(dispatch);
+    if (dispatch->state == DS_PENDING_SESSION) {
+        char* argv[] = {
+            "knit-resume-session", oid_to_hex(&job->object.oid), NULL
+        };
+        *fd = spawn(dispatch, argv, 1);
+        dispatch->state = DS_SESSION;
+    } else if (dispatch->state == DS_SESSION) {
+        // Somewhat awkward script to wrap invocation in a production.
+        char script[1024];
+        snprintf(script, sizeof(script),
+                 "knit-remix-production --set-job %1$s"
+                 " --wrap-invocation `knit-close-session %1$s`",
+                 oid_to_hex(&job->object.oid));
+        char* argv[] = { "sh", "-c", script, NULL };
+        *fd = spawn(dispatch, argv, 0);
+        dispatch->state = DS_RUNNING;
+    } else if (dispatch->state == DS_LAMEDUCK) {
+        if (dispatch->notify) {
+            // We happened to receive requests for this job after sending out
+            // notifications. Instead of deallocating our dispatch, run it again
+            // with remaining notifications.
+            dispatch_job(job, fd);
+        } else {
+            dispatch->state = DS_DONE;
+        }
+    } else {
+        return error("unexpected eof");
+    }
+    return 0;
+}
+
+static int notify_completion(const char* prd_hex, struct notify_list** list_p) {
+    int ret = 0;
+    while (*list_p) {
+        struct dispatch* dispatch = (*list_p)->flow_job->object.extra;
+        assert(dispatch);
+        assert(dispatch->state == DS_SESSION);
+        assert(dispatch->writefd >= 0);
+        if (dprintf(dispatch->writefd, "%s\n", prd_hex) < 0)
+            ret = error_errno("cannot notify session of completion");
+
+        struct notify_list* tmp = (*list_p)->next;
+        free(*list_p);
+        *list_p = tmp;
+    }
+    return ret;
+}
+
+// Process a line of output from a dispatch process. Sets *req_job if it should
+// be scheduled for dispatch.
+static int handle_output(struct job* job, const char* line,
+                         struct job** req_job) {
+    struct dispatch* dispatch = job->object.extra;
+    assert(dispatch);
+    if (dispatch->state == DS_RUNNING) {
+        if (!strcmp(line, "session")) {
+            dispatch->state = DS_PENDING_SESSION;
+        } else {
+            dispatch->state = DS_LAMEDUCK;
+            // If no one is waiting for this dispatch, assume it is the root job
+            // and emit its production.
+            if (!dispatch->notify)
+                puts(line);
+            return notify_completion(line, &dispatch->notify);
+        }
+    } else if (dispatch->state == DS_SESSION) {
+        struct object_id oid;
+        if (strlen(line) != KNIT_HASH_HEXSZ || hex_to_oid(line, &oid) < 0)
+            return error("invalid job hash");
+        *req_job = get_job(&oid);
+    } else {
+        return error("unexpected output");
+    }
+    return 0;
+}
+
+static void create_dispatch_and_pollfd(struct job* job, struct pollfd* pfds,
+                                       struct job** jobs, nfds_t* nfds) {
+    assert(!job->object.extra);
+    if (*nfds + 1 >= MAX_DISPATCHES)
+        die("too many dispatches");
+
+    struct dispatch* dispatch = xmalloc(sizeof(*dispatch));
+    memset(dispatch, 0, sizeof(*dispatch));
+    dispatch->writefd = -1;
+    job->object.extra = dispatch;
+
+    dispatch_job(job, &pfds[*nfds].fd);
+    pfds[*nfds].events = POLLIN;
+    jobs[*nfds] = job;
+    (*nfds)++;
+}
+
+static int cleanup_dispatch_process(struct dispatch* dispatch,
+                                    struct pollfd* pfd) {
+    close(pfd->fd);
+    pfd->fd = -1;
+    close(dispatch->writefd);
+    dispatch->writefd = -1;
+
+    int status;
+    int rc;
+    do {
+        rc = waitpid(dispatch->pid, &status, 0);
+    } while (rc < 0 && errno == EINTR);
+    if (rc < 0)
+        die_errno("waitpid failed");
+    dispatch->pid = 0;
+
+    if (WIFEXITED(status)) {
+        int code = WEXITSTATUS(status);
+        if (code)
+            return error("process died with error code %d", code);
+    } else if (WIFSIGNALED(status)) {
+        return error("process died of signal %d", WTERMSIG(status));
+    }
+    return 0;
+}
+
+// Main poll loop iteration. Returns -1 on error, 1 to process the next index,
+// or 0 to reprocess the current index.
+static int poll_tick(struct pollfd* pfds, struct job** jobs,
+                     nfds_t* nfds, size_t i) {
+    struct dispatch* dispatch = jobs[i]->object.extra;
+    assert(dispatch);
+    size_t size = strlen(dispatch->buf);
+    ssize_t nr = read(pfds[i].fd, dispatch->buf + size,
+                      sizeof(dispatch->buf) - size);
+    if (nr < 0) {
+        if (errno == EINTR || errno == EAGAIN)
+            return 0;
+        return error_errno("read failed");
+    } else if (nr == 0) {
+        if (cleanup_dispatch_process(dispatch, &pfds[i]) < 0)
+            return -1;
+
+        if (*dispatch->buf)
+            return error("unterminated line");
+
+        if (handle_eof(jobs[i], &pfds[i].fd) < 0)
+            return -1;
+        if (dispatch_is_dead(dispatch)) {
+            free_dispatch(dispatch);
+            jobs[i]->object.extra = NULL;
+            (*nfds)--;
+            if (i < *nfds) {
+                memcpy(&pfds[i], &pfds[*nfds], sizeof(*pfds));
+                jobs[i] = jobs[*nfds];
+            }
+            return 0;
+        }
+        return 1;
+    }
+
+    if (memchr(dispatch->buf + size, '\0', nr))
+        return error("illegal NUL byte");
+    size += nr;
+
+    char* nl;
+    while ((nl = memchr(dispatch->buf, '\n', size))) {
+        *nl = '\0';
+        struct job* req_job = NULL;
+        if (handle_output(jobs[i], dispatch->buf, &req_job) < 0)
+            return -1;
+        if (req_job) {
+            struct dispatch* req_dispatch = req_job->object.extra;
+            if (!req_dispatch) {
+                create_dispatch_and_pollfd(req_job, pfds, jobs, nfds);
+                req_dispatch = req_job->object.extra;
+            }
+            dispatch_add_session(req_dispatch, jobs[i]);
+        }
+        size_t off = nl + 1 - dispatch->buf;
+        memmove(dispatch->buf, dispatch->buf + off, size - off);
+        size -= off;
+    }
+
+    if (size >= sizeof(dispatch->buf))
+        return error("output too long");
+    dispatch->buf[size] = '\0';
+
+    return 1;
+}
+
+static void die_usage(char* arg0) {
+    fprintf(stderr, "usage: %s <job>\n", arg0);
+    exit(1);
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2)
+        die_usage(argv[0]);
+
+    struct job* job = peel_job(argv[1]);
+    if (!job)
+        exit(1);
+
+    // To avoid races only one instance may be running for any knit directory.
+    if (snprintf(sched_lockfile, PATH_MAX,
+                 "%s/scheduler.lock", get_knit_dir()) >= PATH_MAX)
+        die("lock path too long");
+    if (acquire_lockfile(sched_lockfile) < 0)
+        return -1;
+    atexit(unlock_sched);
+
+    struct sigaction act = { .sa_handler = sighandler };
+    sigaction(SIGINT, &act, NULL);
+    sigaction(SIGTERM, &act, NULL);
+
+    struct pollfd pfds[MAX_DISPATCHES];
+    struct job* jobs[MAX_DISPATCHES];
+    nfds_t nfds = 0;
+    create_dispatch_and_pollfd(job, pfds, jobs, &nfds);
+
+    while (nfds > 0) {
+        if (poll(pfds, nfds, -1) < 0) {
+            if (errno == EINTR || errno == EAGAIN)
+                continue;
+            die_errno("poll failed");
+        }
+
+        nfds_t i = 0;
+        while (i < nfds) {
+            if (!pfds[i].revents) {
+                i++;
+                continue;
+            }
+            assert(!(pfds[i].revents & POLLNVAL));
+            // On EOF we observe POLLHUP without POLLIN.
+            if (pfds[i].revents & (POLLIN | POLLHUP)) {
+                int rc = poll_tick(pfds, jobs, &nfds, i);
+                if (rc < 0)
+                    exit(1);
+                i += rc;
+            } else {
+                die("unhandled poll revent");
+            }
+        }
+    }
+
+    return 0;
+}

commit 66b70bb27ad8a9274ebcf935957345b55cdee482

Restructure caching
Rewrite cache logic in C and call it from knit-schedule-jobs instead of
knit-dispatch-job. This restores caching of flow jobs that regressed in
commit def451566cbba67527b181c296b08441f564cb52.

We remove some optimistic cache reads and verification on cache writes,
since they are inherently racey and unnecessary for correctness. We also
expect minimal cache contention with the scheduler.

Expand job parsing to determine the process and if it is nocache.
knit-dispatch-job no longer (inefficiently) performs these tasks.
Combined with the more efficient caching implementation, tests run
almost twice as fast.

knit-cache is no longer used, but it has been ported to C anyway.

knit-dispatch-job is increasingly vestigial, and eventually its
functionality may fully move into knit-schedule-jobs and knit-exec-cmd.

Makefile
@@ -22,7 +22,7 @@ SCRIPTS = \
 	$(patsubst %.sh,%,$(wildcard *.sh)) \
 	$(patsubst %.pl,%,$(wildcard *.pl))
 
-OBJS = alloc.o hash.o invocation.o job.o lexer.o object.o production.o resource.o session.o spec.o util.o
+OBJS = alloc.o cache.o hash.o invocation.o job.o lexer.o object.o production.o resource.o session.o spec.o util.o
 
 all: $(BIN) $(SCRIPTS)
 

cache.c
@@ -1,0 +1,96 @@
+#include "cache.h"
+
+#include "hash.h"
+#include "util.h"
+
+// TODO overlaps with object file functions in hash.c
+
+static char* cache_path(const struct job* job) {
+    const char* hex = oid_to_hex(&job->object.oid);
+    static char filename[PATH_MAX];
+    if (snprintf(filename, PATH_MAX, "%s/cache/%c%c/%s",
+                 get_knit_dir(), hex[0], hex[1], &hex[2]) >= PATH_MAX)
+        die("path too long");
+    return filename;
+}
+
+int open_cache_file(const struct job* job) {
+    if (job->is_nocache)
+        return -1;
+    int fd = open(cache_path(job), O_RDONLY);
+    if (fd < 0 && errno != ENOENT)
+        warning_errno("could not open cache file");
+    return fd;
+}
+
+struct production* read_cache(const struct job* job) {
+    int fd = open_cache_file(job);
+    if (fd < 0)
+        return NULL;
+
+    char buf[KNIT_HASH_HEXSZ + 2];
+    int rc;
+    size_t len = 0;
+    do {
+        rc = read(fd, buf + len, sizeof(buf) - len);
+        if (rc < 0 && errno == EINTR)
+            continue;
+        len += rc;
+    } while (rc > 0 && len < sizeof(buf));
+    if (rc < 0)
+        warning_errno("read cache error");
+    close(fd);
+
+    struct object_id oid;
+    if (len != KNIT_HASH_HEXSZ + 1 || buf[KNIT_HASH_HEXSZ] != '\n' ||
+            hex_to_oid(buf, &oid) < 0) {
+        warning("invalid cached production in %s", cache_path(job));
+        return NULL;
+    }
+
+    return get_production(&oid);
+}
+
+static int mkstemp_mode(char* template, mode_t mode) {
+    int fd = mkstemp(template);
+    if (fd >= 0) {
+        mode_t orig_mask = umask(0);
+        umask(orig_mask);
+        fchmod(fd, mode & ~orig_mask);
+    }
+    return fd;
+}
+
+static int move_temp_to_file(const char* tmpfile, const char* filename) {
+    int ret = rename(tmpfile, filename);
+    if (ret < 0 && errno == ENOENT) {
+        // Retry after trying to create the cache subdirectory.
+        char* dir = strrchr(filename, '/');
+        *dir = '\0';
+        mkdir(filename, 0777);
+        *dir = '/';
+        return rename(tmpfile, filename);
+    }
+    return ret;
+}
+
+int write_cache(const struct job* job, const struct production* prd) {
+    if (job->is_nocache)
+        return 0;
+
+    char tmpfile[PATH_MAX];
+    if (snprintf(tmpfile, PATH_MAX, "%s/tmp-XXXXXX", get_knit_dir()) >= PATH_MAX)
+        return error("path too long");
+    int fd = mkstemp_mode(tmpfile, 0666);
+    if (fd < 0)
+        return error_errno("cannot open cache temp file");
+    if (dprintf(fd, "%s\n", oid_to_hex(&prd->object.oid)) < 0)
+        return error_errno("write failed");
+    if (close(fd) < 0)
+        return error_errno("close failed");
+
+    if (move_temp_to_file(tmpfile, cache_path(job)) < 0)
+        return error_errno("failed to rename cache file");
+
+    return 0;
+}

cache.h
@@ -1,0 +1,10 @@
+#pragma once
+
+#include "job.h"
+#include "production.h"
+
+struct production* read_cache(const struct job* job);
+int write_cache(const struct job* job, const struct production* prd);
+// Open cache file for read. Return its file descriptor, or -1 if nonexistent or
+// on error.
+int open_cache_file(const struct job* job);

job.c
@@ -1,5 +1,19 @@
 #include "job.h"
 
+const char* job_process_name(enum job_process process) {
+    switch (process) {
+    case JOB_PROCESS_CMD:
+        return "cmd";
+    case JOB_PROCESS_FLOW:
+        return "flow";
+    case JOB_PROCESS_IDENTITY:
+        return "identity";
+    case JOB_PROCESS_INVALID:
+        break;
+    }
+    die("invalid job process");
+}
+
 struct job* get_job(const struct object_id* oid) {
     return intern_object(oid, OBJ_JOB, sizeof(struct job));
 }
@@ -28,12 +42,32 @@ int parse_job_bytes(struct job* job, void* data, size_t size) {
         list->name = strdup(in->name);
         list->next = NULL;
 
+        enum job_process process = JOB_PROCESS_INVALID;
+        if (!strcmp(list->name, JOB_INPUT_NOCACHE)) {
+            job->is_nocache = 1;
+        } else if (!strcmp(list->name, JOB_INPUT_CMD)) {
+            process = JOB_PROCESS_CMD;
+        } else if (!strcmp(list->name, JOB_INPUT_FLOW)) {
+            process = JOB_PROCESS_FLOW;
+        } else if (!strcmp(list->name, JOB_INPUT_IDENTITY)) {
+            process = JOB_PROCESS_IDENTITY;
+        }
+
+        if (process != JOB_PROCESS_INVALID) {
+            if (job->process != JOB_PROCESS_INVALID)
+                return error("job has multiple processes");
+            job->process = process;
+        }
+
         off += sizeof(*in) + pathlen + 1;
         *list_p = list;
         list_p = &list->next;
     }
     if (off != size)
         return error("trailing job data");
+
+    if (job->process == JOB_PROCESS_INVALID)
+        return error("job missing process");
 
     job->object.is_parsed = 1;
     return 0;

job.h
@@ -3,9 +3,20 @@
 #include "object.h"
 #include "resource.h"
 
+enum job_process {
+    JOB_PROCESS_INVALID = 0,  // never used by parsed jobs
+    JOB_PROCESS_CMD,
+    JOB_PROCESS_FLOW,
+    JOB_PROCESS_IDENTITY,
+};
+
+const char* job_process_name(enum job_process process);
+
 struct job {
     struct object object;
     struct resource_list* inputs;
+    enum job_process process;
+    unsigned is_nocache : 1;
 };
 
 struct job* get_job(const struct object_id* oid);

knit-cache.c
@@ -1,0 +1,28 @@
+#include "cache.h"
+#include "spec.h"
+
+static void die_usage(const char* arg0) {
+    fprintf(stderr, "usage: %s <job> [<production>]\n", arg0);
+    exit(1);
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2 || argc > 3)
+        die_usage(argv[0]);
+
+    struct job* job = peel_job(argv[1]);
+    if (!job)
+        exit(1);
+    if (argc == 2) {
+        struct production* prd = read_cache(job);
+        if (!prd)
+            exit(1);
+        puts(oid_to_hex(&prd->object.oid));
+    } else {
+        struct production* prd = peel_production(argv[2]);
+        if (!prd || write_cache(job, prd) < 0)
+            exit(1);
+    }
+
+    return 0;
+}

knit-cache.sh
@@ -1,50 +1,0 @@
-#!/bin/bash -e
-
-. knit-bash-setup
-
-unset prd
-if [[ $# -eq 2 ]]; then
-    prd="$2"
-elif [[ $# -ne 1 ]]; then
-    echo "usage: $0 <job> [<production>]" >&2
-    exit 1
-fi
-job="$1"
-
-cache="$KNIT_DIR/cache/${job:0:2}/${job:2}"
-
-# As an optimization attempt to read the cache outside of the lock. This
-# should be safe because we assume the cache is set once atomically.
-if [[ -z $prd && -e $cache ]]; then
-    echo "$(< "$cache")"
-    exit
-fi
-
-mkdir -p "$(dirname "$cache")"
-exec 3> "$cache.lock"
-flock 3
-
-rc=0
-if [[ -z $prd ]]; then
-    if [[ -e $cache ]]; then
-        echo "$(< "$cache")"
-    else
-        rc=1
-    fi
-else
-    if [[ -e $cache ]]; then
-        existing="$(< $cache)"
-        if [[ $existing == $prd ]]; then
-            echo "warning: cache redundantly set" >&2
-        else
-            echo "Job already cached with production $existing" >&2
-            rc=1
-        fi
-    else
-        echo "$prd" > $cache.tmp
-        mv $cache.tmp $cache
-    fi
-fi
-
-rm "$cache.lock"
-exit "$rc"

knit-dispatch-job.sh
@@ -2,8 +2,9 @@
 
 . knit-bash-setup
 
-[[ $# -eq 1 ]]
-job="$1"
+[[ $# -eq 2 ]]
+process="$1"
+job="$2"
 
 scratch="$KNIT_DIR/scratch/$job"
 
@@ -60,44 +61,17 @@ process_cmd() {
     knit-remix-production "${remix_opts[@]}"
 }
 
-finish() {
-    if [[ -z $nocache ]]; then
-        knit-cache "$job" "$prd"
-    fi
-    echo "$prd"
-    exit
-}
-
-unset prd
-if prd=$(knit-cache "$job"); then
-    echo -e "!!cache-hit\t$job\t$prd" >&2
-    echo "$prd"
-    exit
-fi
-
-# This quick-and-dirty invocation of knit-cat-file could be optimized away, but
-# it would require more lines of code :)
-unset nocache
-if knit-cat-file -p "$job" | cut -f2- | grep -qxF .knit/nocache; then
-    nocache=1
-fi
-
-while read -r input; do
-    if [[ $input == .knit/cmd ]]; then
+case $process in
+    cmd)
         lock_scratch
-        if prd=$(knit-cache "$job"); then
-            echo -e "!!cache-hit-slow\t$job\t$prd" >&2
-            echo "$prd"
-            exit
-        fi
         unpack_job
 
         prd=$(process_cmd)
 
         # TODO when to keep scratch dir?
         rm -rf "$scratch"
-        finish
-    elif [[ $input == .knit/flow ]]; then
+        ;;
+    flow)
         set -o pipefail
         # By convention, the session name is the same as the flow job; any
         # existing session should be from a previous identical
@@ -111,12 +85,14 @@ while read -r input; do
         fi
         echo session
         exit
-    elif [[ $input == .knit/identity ]]; then
+        ;;
+    identity)
         prd=$(knit-remix-production --set-job "$job" --copy-job-inputs "$job" \
             --remove-prefix .knit/ --set-output ".knit/ok=$(empty_res)")
-        finish
-    fi
-done < <(knit-cat-file -p "$job" | cut -f2-)
+        ;;
+    *)
+        echo "Unsupported process $process" >&2
+        exit 1
+esac
 
-echo "Unsupported job $job" >&2
-exit 1
+echo "$prd"

knit-init.sh
@@ -11,6 +11,6 @@ if [[ $# -eq 1 && $KNIT_DIR != /* ]]; then
     KNIT_DIR="$1/$KNIT_DIR"
 fi
 
-mkdir -p "$KNIT_DIR"/{objects,scratch,sessions}
+mkdir -p "$KNIT_DIR"/{cache,objects,scratch,sessions}
 
 echo Initialized Knit repository in "$KNIT_DIR" >&2

knit-schedule-jobs.c
@@ -11,8 +11,10 @@
 // Each outstanding job, or dispatch, maintains which sessions are waiting upon
 // it. Upon completion, we notify these sessions of the resulting production.
 
+#include "cache.h"
 #include "hash.h"
 #include "job.h"
+#include "production.h"
 #include "spec.h"
 #include "util.h"
 
@@ -128,8 +130,20 @@ static void dispatch_job(struct job* job, int* fd) {
     struct dispatch* dispatch = job->object.extra;
     assert(dispatch);
     dispatch->state = DS_RUNNING;
-    char* argv[] = { "knit-dispatch-job", oid_to_hex(&job->object.oid), NULL };
-    *fd = spawn(dispatch, argv, 0);
+
+    *fd = open_cache_file(job);
+    if (*fd < 0) {
+        // Copy from job_process_name() for const correctness.
+        char process[20];
+        if (!memccpy(process, job_process_name(job->process),
+                     '\0', sizeof(process)))
+            die("process name overflow");
+
+        char* argv[] = {
+            "knit-dispatch-job", process, oid_to_hex(&job->object.oid), NULL
+        };
+        *fd = spawn(dispatch, argv, 0);
+    }
     // This violates encapsulation, but it lets us easily reactivate a pollfd
     // from its dispatch.
     dispatch->pfdp = fd;
@@ -169,15 +183,22 @@ static int handle_eof(struct job* job, int* fd) {
     return 0;
 }
 
+static int write_production_to_session(const struct dispatch* dispatch,
+                                       const char* prd_hex) {
+    assert(dispatch->state == DS_SESSION);
+    assert(dispatch->writefd >= 0);
+    if (dprintf(dispatch->writefd, "%s\n", prd_hex) < 0)
+        return error_errno("cannot notify session of completion");
+    return 0;
+}
+
 static int notify_completion(const char* prd_hex, struct notify_list** list_p) {
     int ret = 0;
     while (*list_p) {
         struct dispatch* dispatch = (*list_p)->flow_job->object.extra;
         assert(dispatch);
-        assert(dispatch->state == DS_SESSION);
-        assert(dispatch->writefd >= 0);
-        if (dprintf(dispatch->writefd, "%s\n", prd_hex) < 0)
-            ret = error_errno("cannot notify session of completion");
+        if (write_production_to_session(dispatch, prd_hex) < 0)
+            ret = -1;
 
         if (dispatch->num_outstanding-- == MAX_DISPATCHES_PER_SESSION) {
             assert(*dispatch->pfdp < 0);
@@ -201,6 +222,20 @@ static int handle_output(struct job* job, const char* line,
         if (!strcmp(line, "session")) {
             dispatch->state = DS_PENDING_SESSION;
         } else {
+            // If we were running a process (knit-dispatch-job) then we should
+            // cache the result. Otherwise the result itself was from the cache.
+            if (dispatch->pid) {
+                struct object_id oid;
+                if (strlen(line) != KNIT_HASH_HEXSZ || hex_to_oid(line, &oid) < 0)
+                    return error("invalid production hash");
+                struct production* prd = get_production(&oid);
+                if (write_cache(job, prd) < 0)
+                    return -1;
+            } else {
+                fprintf(stderr, "!!cache-hit\t%s\t%s\n",
+                        oid_to_hex(&job->object.oid), line);
+            }
+
             dispatch->state = DS_LAMEDUCK;
             // If no one is waiting for this dispatch, assume it is the root job
             // and emit its production.
@@ -240,8 +275,13 @@ static int cleanup_dispatch_process(struct dispatch* dispatch,
                                     struct pollfd* pfd) {
     close(pfd->fd);
     pfd->fd = -1;
-    close(dispatch->writefd);
-    dispatch->writefd = -1;
+    if (dispatch->writefd >= 0) {
+        close(dispatch->writefd);
+        dispatch->writefd = -1;
+    }
+
+    if (!dispatch->pid)  // no process (read from cache file)
+        return 0;
 
     int status;
     int rc;
@@ -308,6 +348,8 @@ static int poll_tick(struct pollfd* pfds, struct job** jobs,
         if (handle_output(jobs[i], dispatch->buf, &req_job) < 0)
             return -1;
         if (req_job) {
+            if (parse_job(req_job) < 0)
+                return -1;
             struct dispatch* req_dispatch = req_job->object.extra;
             if (!req_dispatch) {
                 create_dispatch_and_pollfd(req_job, pfds, jobs, nfds);
@@ -344,7 +386,7 @@ int main(int argc, char** argv) {
         die_usage(argv[0]);
 
     struct job* job = peel_job(argv[1]);
-    if (!job)
+    if (!job || parse_job(job) < 0)
         exit(1);
 
     // To avoid races only one instance may be running for any knit directory.
@@ -352,7 +394,7 @@ int main(int argc, char** argv) {
                  "%s/scheduler.lock", get_knit_dir()) >= PATH_MAX)
         die("lock path too long");
     if (acquire_lockfile(sched_lockfile) < 0)  // leaks returned fd
-        return -1;
+        exit(1);
     atexit(unlock_sched);
 
     struct sigaction act = { .sa_handler = sighandler };