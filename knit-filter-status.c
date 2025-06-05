2a8c77e6fbc3a00721a484a5b1e2076a489319fb
Joe Mou <dev@mou.fo>
2025-06-04 22:43:01 -0400

    Teach knit-filter-status to launch knit-schedule-jobs pipeline
    Essentially ports the file descriptor juggling and pipeline from
    knit-run-plan to knit-filter-status. It's mostly a wash from a
    readability perspective, but it is a stepping stone to replacing the run
    pipe with a socket, which hopefully is easier to reason about.

--- knit-filter-status.c
+++ knit-filter-status.c
@@ -14,25 +14,31 @@ static void update_columns([[maybe_unused]] int signo) {
 static void maybe_clear_line(int clear_next) {
     static int needs_clear = 0;
     if (needs_clear)
-        fputs("\r\e[K", stdout);  // \e[K is ANSI Erase in Line (to right)
+        fputs("\r\e[K", stderr);  // \e[K is ANSI Erase in Line (to right)
     needs_clear = clear_next;
 }
 
+static int enable_status;
+
 [[gnu::format(printf, 1, 2)]]
-static void emitf(const char* fmt, ...) {
-    va_list argp;
-    va_start(argp, fmt);
-    maybe_clear_line(0);
-    vfprintf(stdout, fmt, argp);
-    fflush(stdout);
-    va_end(argp);
+static void statusf(const char* fmt, ...) {
+    if (enable_status) {
+        va_list argp;
+        va_start(argp, fmt);
+        maybe_clear_line(0);
+        vfprintf(stderr, fmt, argp);
+        fflush(stderr);
+        va_end(argp);
+    }
 }
 
-static void status(const char* buf) {
-    maybe_clear_line(1);
-    // Keep the cursor on this line so it will be replaced on the next call.
-    fputs(buf, stdout);
-    fflush(stdout);
+static void hardstatus(const char* buf) {
+    if (enable_status) {
+        maybe_clear_line(1);
+        // Keep the cursor on this line so it will be replaced on the next call.
+        fputs(buf, stderr);
+        fflush(stderr);
+    }
 }
 
 struct step_list {
@@ -146,7 +152,7 @@ static void progress_handle_step(struct progress* progress,
                                                 parsed->session);
     if (!session) {
         session = create_session(&progress->sessions, parsed->session);
-        emitf("Session %s\n", session->name);
+        statusf("Session %s\n", session->name);
     }
 
     if (!strcmp(parsed->state, "-")) {
@@ -201,18 +207,85 @@ static void format_status(const struct progress* progress,
 static void print_summary(struct progress* progress) {
     float duration = (float)(progress->last_ns - progress->first_ns) / 1000000000;
     const char* plural = progress->num_final > 1 ? "s" : "";
-    emitf("Ran %u job%s (%u cached) in %.1f seconds\n",
-          progress->num_final, plural, progress->num_cached, duration);
+    statusf("Ran %u job%s (%u cached) in %.1f seconds\n",
+            progress->num_final, plural, progress->num_cached, duration);
+}
+
+static int spawn(char** argv, int infd, int* errfdp) {
+    int fds[2];
+    if (pipe(fds) < 0)
+        die_errno("pipe failed");
+    fcntl(fds[0], F_SETFD, FD_CLOEXEC);
+    fcntl(fds[1], F_SETFD, FD_CLOEXEC);
+
+    pid_t pid = fork();
+    if (pid < 0)
+        die_errno("fork failed");
+    if (!pid) {
+        dup2(infd, STDIN_FILENO);
+        dup2(fds[1], STDERR_FILENO);
+        execvp(argv[0], argv);
+        die_errno("execvp failed");
+    }
+
+    close(fds[1]);
+    *errfdp = fds[0];
+    return pid;
+}
+
+[[noreturn]]
+static void exit_like_child(int pid) {
+    int status;
+    int rc;
+    do {
+        rc = waitpid(pid, &status, 0);
+    } while (rc < 0 && errno == EINTR);
+    if (rc < 0)
+        die_errno("waitpid failed");
+
+    if (WIFEXITED(status))
+        exit(WEXITSTATUS(status));
+    if (WIFSIGNALED(status))
+        exit(128 + WTERMSIG(status));
+    die("unexpected subprocess status");
 }
 
 static void die_usage(const char* arg0) {
-    fprintf(stderr, "usage: %s < <!!-lines>\n", arg0);
+    int len = strlen(arg0);
+    fprintf(stderr, "usage: %*s [--off|--dumb|--on] <input-pipe>\n", len, arg0);
+    fprintf(stderr, "       %*s <child-process> [<args>]...\n", len, "");
     exit(1);
 }
 
 int main(int argc, char** argv) {
-    if (argc != 1)
+    if (argc < 4)
         die_usage(argv[0]);
+
+    // Open input pipe for read/write to prevent waiting for writer.
+    // See https://unix.stackexchange.com/a/496812
+    int infd = open(argv[2], O_RDWR);
+    if (infd < 0)
+        die_errno("cannot open input pipe %s", argv[2]);
+    fcntl(infd, F_SETFD, FD_CLOEXEC);
+
+    if (!strcmp(argv[1], "--off")) {
+        // Do not filter, just exec the child (with the input pipe set).
+        dup2(infd, STDIN_FILENO);
+        execvp(argv[3], &argv[3]);
+        die_errno("execvp failed");
+    } else if (!strcmp(argv[1], "--dumb")) {
+        enable_status = 0;
+    } else if (!strcmp(argv[1], "--on")) {
+        enable_status = 1;
+    } else {
+        die_usage(argv[0]);
+    }
+
+    int child_errfd;
+    int child_pid = spawn(&argv[3], infd, &child_errfd);
+    close(infd);
+
+    FILE* child_stderr = fdopen(child_errfd, "r");
 
     struct sigaction act = {
         .sa_handler = update_columns,
@@ -227,10 +300,11 @@ int main(int argc, char** argv) {
     char* line = NULL;
     size_t size = 0;
     ssize_t nread;
-    while (errno = 0, (nread = getline(&line, &size, stdin)) > 0) {
+    while (errno = 0, (nread = getline(&line, &size, child_stderr)) > 0) {
         // Pass through incomplete lines or not starting with !!
         if (line[nread - 1] != '\n' || line[0] != '!' || line[1] != '!') {
-            emitf("%s", line);
+            maybe_clear_line(0);
+            fputs(line, stderr);
             continue;
         }
         line[nread - 1] = '\0';
@@ -245,7 +319,7 @@ int main(int argc, char** argv) {
 
             char buf[columns + 1];
             format_status(&progress, buf, sizeof(buf));
-            status(buf);
+            hardstatus(buf);
         }
     }
     if (nread < 0 && errno > 0)
@@ -254,6 +328,7 @@ int main(int argc, char** argv) {
     if (progress.num_final > 0)
         print_summary(&progress);
 
+    fclose(child_stderr);
     // leak line and sessions
-    return 0;
+    exit_like_child(child_pid);
 }
--- knit-run-plan.sh
+++ knit-run-plan.sh
@@ -5,16 +5,16 @@
 usage() { echo "usage: $0 [-f <plan>] [--no-filter] [<plan-job-options>]" >&2; exit 1; }
 
 if [[ -t 1 ]]; then
-    filter=knit-filter-status
+    filter=--on
 else
-    filter='grep -v ^!!'
+    filter=--dumb
 fi
 
 plan=plan.knit
 while [[ $# -gt 0 ]]; do
     case "$1" in
         -f) plan="$2"; shift;;
-        --no-filter) filter=cat;;
+        --no-filter) filter=--off;;
         *) break;;
     esac
     shift
@@ -37,12 +37,7 @@ while read -r word oid; do
     else
         echo "Unrecognized word $word" >&2
     fi
-# Open run.pipe for read/write to prevent waiting for writer.
-# See https://unix.stackexchange.com/a/496812
-# We do some crazy file descriptor juggling to filter stderr.
-# See https://stackoverflow.com/a/52575213/13773246
-# The `|| true` prevents grep's exit code from terminating the entire script.
-done < <({ knit-schedule-jobs "$job" 1<> "$KNIT_DIR/run.pipe" 2>&1 >&3 3>&- | { $filter || true; } >&2 3>&-; } 3>&1)
+done < <(knit-filter-status $filter "$KNIT_DIR/run.pipe" knit-schedule-jobs "$job")
 
 # TODO truncate history
 echo "$prd" | tee -a "$KNIT_DIR/history"
